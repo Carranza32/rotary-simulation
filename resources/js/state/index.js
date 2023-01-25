a14cade80cf0a987852e097723f61666c60b3874
Mario Carranza <mario.carranza996@gmail.com>
2023-01-25 09:47:07 -0600

    vuex

---
+++ resources/js/state/index.js
@@ -0,0 +1,15 @@
+const store = ({
+    state() {
+        return {
+            step1: {
+                name: '',
+                last_name: '',
+                work: '',
+                address: '',
+                phone: '',
+            },
+        }
+    }
+})
+
+export default store
