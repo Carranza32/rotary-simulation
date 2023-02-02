065de0a2ff7cc71675807411f9e4b1101e825217
Mario Carranza <mario.carranza996@gmail.com>
2023-02-02 12:33:24 -0600

    group routes

--- routes/web.php
+++ routes/web.php
@@ -29,23 +29,25 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::controller(RotaryFormController::class)->name('simulation.')->group(function () {
+        Route::get('/simulation', 'form')->name('form');
+
+        Route::post('/simulation/step/1', 'saveStep1')->name('save.step1');
+        Route::post('/simulation/step/2', 'saveStep2')->name('save.step2');
+        Route::post('/simulation/step/3', 'saveStep3')->name('save.step3');
+        Route::post('/simulation/step/4', 'saveStep4')->name('save.step4');
+        Route::post('/simulation/step/5', 'saveStep5')->name('save.step5');
+        Route::post('/simulation/step/6', 'saveStep6')->name('save.step6');
+        Route::post('/simulation/step/7', 'saveStep7')->name('save.step7');
+        Route::post('/simulation/step/8', 'saveStep8')->name('save.step8');
+        Route::post('/simulation/step/9', 'saveStep9')->name('save.step9');
+        Route::post('/simulation/step/10', 'saveStep10')->name('save.step10');
+        Route::post('/simulation/step1/1', 'saveStep11')->name('save.step11');
+        Route::post('/simulation/st2ep/1', 'saveSte2p1')->name('save.step12');
+    });
 });
 
-Route::controller(RotaryFormController::class)->name('simulation.')->group(function () {
-    Route::get('/simulation', 'form')->name('form');
 
-    Route::post('/simulation/step/1', 'saveStep1')->name('save.step1');
-    Route::post('/simulation/step/2', 'saveStep2')->name('save.step2');
-    Route::post('/simulation/step/3', 'saveStep3')->name('save.step3');
-    Route::post('/simulation/step/4', 'saveStep4')->name('save.step4');
-    Route::post('/simulation/step/5', 'saveStep5')->name('save.step5');
-    Route::post('/simulation/step/6', 'saveStep6')->name('save.step6');
-    Route::post('/simulation/step/7', 'saveStep7')->name('save.step7');
-    Route::post('/simulation/step/8', 'saveStep8')->name('save.step8');
-    Route::post('/simulation/step/9', 'saveStep9')->name('save.step9');
-    Route::post('/simulation/step/10', 'saveStep10')->name('save.step10');
-    Route::post('/simulation/step1/1', 'saveStep11')->name('save.step11');
-    Route::post('/simulation/st2ep/1', 'saveSte2p1')->name('save.step12');
-});
 
 require __DIR__.'/auth.php';
